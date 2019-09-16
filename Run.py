499004f1b1941f8fff9e887e6981337ab7fb2550
Amir Daaee <amir.daaee@gmail.com>
2019-09-16 16:00:15 +0430

    Some changes
    multi-threading based ping
    separated authentication from config file
    other minor improvements

--- Run.py
+++ Run.py
@@ -3,28 +3,38 @@ import subprocess
 import re
 import os
 import yaml
+from multiprocessing.pool import ThreadPool
 
+# ---------------- Directory preparing
 base_dir_path = os.path.dirname(os.path.realpath(__file__))
 conf_file_path = os.path.join(base_dir_path, 'conf.yaml')
+auth_file_path = os.path.join(base_dir_path, 'auth.yaml')
+server_file_path = os.path.join(base_dir_path, 'server.address')
+# ---------------- Load configs
 with open(conf_file_path, 'r') as ymlcfg:
-    cfg = yaml.load(ymlcfg)
-
-address_file = os.path.join(base_dir_path, 'server.address')
-if not os.path.isfile(address_file):
-    print('Creating server address file at ', address_file)
-    with open(address_file, 'w') as _:
+    cfg = yaml.safe_load(ymlcfg)
+# ---------------- Load authentication data
+if not os.path.isfile(auth_file_path):
+    print('Creating auth template at ', auth_file_path)
+    with open(auth_file_path, 'w') as f:
+        yaml.safe_dump({'username': None, 'password': None}, f)
+with open(auth_file_path, 'r') as ymlauth:
+    auth = yaml.safe_load(ymlauth)
+# ---------------- Load Servers
+if not os.path.isfile(server_file_path):
+    print('Creating server address file at ', server_file_path)
+    with open(server_file_path, 'w') as _:
         pass
-
-with open(address_file, 'r') as adds:
+with open(server_file_path, 'r') as adds:
     adds = adds.read()
 adds = adds.split('\n')
 adds = [x.replace(' ', '').split(':') for x in adds if x.replace(' ', '') != '']
 for c, i in enumerate(adds):
     if len(i) == 1:
-        adds[c].append(cfg['personalize']['server-port_default'])
-assert len(adds) > 0, 'No server is defined in {}'.format(address_file)
-
-ping = cfg['personalize']['ping_default']
+        adds[c].append(cfg['server-port_default'])
+assert len(adds) > 0, 'No server is defined in {}'.format(server_file_path)
+# ---------------- Ping servers
+ping = cfg['ping_default']
 def_str = 'y/N'
 if ping:
     def_str = 'Y/n'
@@ -34,23 +44,34 @@ if ping_ == 'n':
     ping = False
 elif ping_ == 'y':
     ping = True
-
-for c, i in enumerate(adds):
-    print(c, ':', i[0] + ':' + i[1], end=' ')
-    if ping:
-        command = "nping --tcp-connect --delay {} -c {} -p {} {}".format(cfg['personalize']['ping_delay'],
-                                                                         cfg['personalize']['ping_count'],
-                                                                         i[1],
-                                                                         i[0])
-        p = subprocess.run(command, shell=True, stdout=subprocess.PIPE).stdout
-        p = re.findall(r"(Avg rtt: )(.+)(TCP)", str(p))
+# .............
+if ping:
+    def ping_time(addr):
+        command_ = "nping --tcp-connect --delay {} -c {} -p {} {}".format(cfg['ping_delay'],
+                                                                          cfg['ping_count'],
+                                                                          addr[1],
+                                                                          addr[0])
+        p_ = subprocess.run(command_, shell=True, stdout=subprocess.PIPE).stdout
+        p_ = re.findall(r"(Avg rtt: )(.+)(TCP)", str(p_))
         try:
-            print('--', p[0][1][:-2])
-        except Exception as E:
-            print('NA ({})'.format(E))
-    else:
-        print(' ')
+            return p_[0][1][:-2]
+        except Exception as E_:
+            return str(E_)
 
+
+    with ThreadPool(max(len(adds), int(cfg['max_ping_threads']))) as tpool:
+        latency = tpool.map(ping_time, adds)
+# ---------------- Print Server list
+for c, i in enumerate(adds):
+    s_ = str(c) + ') ' + i[0] + ':' + i[1]
+    print(s_, end='')
+    if ping:
+        s_ = cfg['server_ping_spaces'] - len(s_)
+        print(' ' * s_, end='')
+        print(latency[c])
+    else:
+        print('')
+# ---------------- Get desired server
 while True:
     c = input('Which?')
     try:
@@ -60,22 +81,23 @@ while True:
         print(E)
 adds = c[0] + ':' + c[1]
 print('Connecting to', adds)
-if cfg['auth']['username'] is None:
+# ---------------- authentication Data
+if auth['username'] is None:
     print('no username is set')
-    print('you can save username in {} at auth:username:'.format(conf_file_path))
-    username = input('username :')
+    print('you can save username in {} at username:'.format(auth_file_path))
+    username = input('username:')
 else:
-    username = cfg['auth']['username']
-    print('username :', username)
+    username = auth['username']
+    print('username:', username)
 
-if cfg['auth']['pass'] is None:
+if auth['password'] is None:
     print('no password is set')
-    print('you can save password in {} at auth:pass:'.format(conf_file_path))
-    passw = input('password :')
+    print('you can save password in {} at password:'.format(auth_file_path))
+    passw = input('password:')
 else:
-    passw = cfg['auth']['pass']
-
-forw = cfg['personalize']['Port-Forward_default']
+    passw = auth['password']
+# ---------------- Set running mode
+forw = cfg['Port-Forward_default']
 def_str = 'y/N'
 if forw:
     def_str = 'Y/n'
@@ -85,9 +107,9 @@ if forw_ == 'n':
     forw = False
 elif forw_ == 'y':
     forw = True
-
+# ---------------- Run
 if forw:
-    port = cfg['personalize']['forward_port']
+    port = cfg['forward_port']
     print("Port-Forward :", port)
     print('-' * 50)
     command = 'printf "%s\\n" yes {} | openconnect --script-tun   --script   "ocproxy   -D   9055" -u {} {}'.format(
